6777ee812fd56b370d6ed9c01f744749501ce1c3
faldubhoomi4 <faldubhoomi4@gmail.com>
2026-02-07 20:58:41 +0530

    add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-import eslint from '@eslint/js'
-import { defineConfig } from 'eslint/config'
-import tseslint from 'typescript-eslint'
 
-export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
-    },
-    {
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-            },
-        },
-        rules: {
-            'no-console': 'off',
-            'dot-notation': 'error',
-        },
-    },
-)
-=======
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
@@ -46,4 +23,4 @@ export default defineConfig(
   },
   
 );
->>>>>>> 44f6f39a923e40cfeb13f53da5c032dc06c35206
+
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 function welcome(name: string) {
-<<<<<<< HEAD
+
     console.log('welcome', name)
 }
 const user = {
@@ -10,13 +10,4 @@ welcome('bh')
 
 const U1 = user.name
 console.log(U1)
-=======
-  console.log("welcome", name);
- 
-  }
- const user = {
-    name : "bhoomi"}
-const U1 = user.name
-welcome("bh");
- 
->>>>>>> 44f6f39a923e40cfeb13f53da5c032dc06c35206
+
